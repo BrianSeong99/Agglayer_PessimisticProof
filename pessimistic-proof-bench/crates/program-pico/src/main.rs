63c763173bf6447256d337948b9b44ea2b8a2928
Brian Seong <BrianSeong.dev@gmail.com>
2025-02-23 09:09:53 -0800

    fix: changing program-pico back from valida scripts

--- pessimistic-proof-bench/crates/program-pico/src/main.rs
+++ pessimistic-proof-bench/crates/program-pico/src/main.rs
@@ -4,33 +4,23 @@ use bincode::Options;
 use pessimistic_proof_core::local_exit_tree::hasher::Keccak256Hasher;
 use pessimistic_proof_core::multi_batch_header::MultiBatchHeader;
 use pessimistic_proof_core::{generate_pessimistic_proof, NetworkState, PessimisticProofOutput};
-use valida_rs::io::InputTape;
-use serde::Deserialize;
 
-valida_rs::entrypoint!(main);
-
-#[no_mangle]
+pico_sdk::entrypoint!(main);
+use pico_sdk::io::{commit_bytes, read_as};
 
 pub fn main() {
-    // Use serde_json deserializer with InputTape for reading input
-    let mut de = serde_json::Deserializer::from_reader(InputTape);
-    let input: ProofInput = ProofInput::deserialize(&mut de).expect("could not parse input");
+    // Read the input states
+    let initial_state = read_as::<NetworkState>();
+    let batch_header = read_as::<MultiBatchHeader<Keccak256Hasher>>();
 
     // Generate the proof
-    let outputs = generate_pessimistic_proof(input.initial_state, &input.batch_header).unwrap();
+    let outputs = generate_pessimistic_proof(initial_state, &batch_header).unwrap();
 
     // Serialize the outputs
     let pp_inputs = PessimisticProofOutput::bincode_options()
         .serialize(&outputs)
         .unwrap();
 
-    // Print the results
-    println!("Proof generated successfully");
-    println!("Output size: {} bytes", pp_inputs.len());
-}
-
-#[derive(Deserialize)]
-pub struct ProofInput {
-    initial_state: NetworkState,
-    batch_header: MultiBatchHeader<Keccak256Hasher>,
+    // Commit the result
+    commit_bytes(&pp_inputs);
 }
